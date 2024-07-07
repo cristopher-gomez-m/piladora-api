325213deed8cc75ee355106d2268700b0d49d578
Charlie22RM <57648343+Charlie22RM@users.noreply.github.com>
2024-07-07 13:55:47 -0500

    update user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Get, Post, Delete, Query, Param, HttpCode } from '@nestjs/common';
+import { Body, Controller, Get, Post, Delete, Query, Param, HttpCode, Put, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './entity/DTO/create-user.dto';
 import { User } from './entity/user.entity';
 import { Paginated, PaginateQuery } from 'nestjs-paginate';
 import { UserDTO } from './entity/DTO/user.dto';
+import { UpdateUserDto } from './entity/DTO/update.user.dto';
 
 @Controller('user') // Establece el prefijo de ruta para todo el controlador
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+
+  
 
   @Get() // Ruta relativa a '/user'
   async getUsers(@Query() query: PaginateQuery): Promise<Paginated<User>> {
@@ -19,6 +23,14 @@ export class UserController {
   async store(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.userService.store(createUserDto);
   }
+
+  @Put(':id')
+    async updateUser(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() updateUserDto: UpdateUserDto
+    ): Promise<User> {
+        return await this.userService.update(id, updateUserDto);
+    }
 
   @Delete(':userId')
   @HttpCode(200)
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,6 +7,7 @@ import { Role } from 'src/role/entity/role.entity';
 import { Status } from 'src/enums/status';
 import { UserDTO } from './entity/DTO/user.dto';
 import { paginate, Paginated, PaginateQuery } from 'nestjs-paginate';
+import { UpdateUserDto } from './entity/DTO/update.user.dto';
 
 @Injectable()
 export class UserService {
@@ -15,17 +16,38 @@ export class UserService {
     private userRepository: Repository<User>,
     @InjectRepository(Role)
     private roleRepository: Repository<Role>,
-  ) {}
+  ) { }
 
-  async findAll(query:PaginateQuery): Promise<Paginated<User>> {
+  async findAll(query: PaginateQuery): Promise<Paginated<User>> {
     const paginatedResults = await paginate(query, this.userRepository, {
       relations: ['role'],
-      sortableColumns:['id'], 
-      select:['id','username','nombre','role','status','fecha_creacion','creado_por'],
+      sortableColumns: ['id'],
+      select: [
+        'id',
+        'username',
+        'nombre',
+        'cedula',
+        'password',
+        'apellido',
+        'status',
+        'fecha_creacion',
+        'creado_por',
+        'role'  // Incluir el rol en la selección
+      ],
       where: { status: Status.A },
     });
-  
-    return  paginatedResults
+
+    // Para cada usuario, selecciona los campos necesarios del rol
+    paginatedResults.data.forEach(user => {
+      if (user.role) {
+        user.role = {
+          id: user.role.id,
+          name: user.role.name
+        } as Role;
+      }
+    });
+
+    return paginatedResults;
   }
 
   async store(createUserDto: CreateUserDto): Promise<User> {
@@ -38,27 +60,65 @@ export class UserService {
       ...createUserDto,
       role,  // asigna el objeto Role en lugar del ID
     });
-    user.status= Status.A;
+    user.status = Status.A;
     user.fecha_creacion = new Date();
     user.creado_por = createUserDto.user_id;
     return await this.userRepository.save(user);
   }
 
-  async delete(id:number){
+  async delete(id: number) {
     try {
-      const user = await this.userRepository.findOneBy({id});
+      const user = await this.userRepository.findOneBy({ id });
       if (!user) {
         throw new NotFoundException(`Usuario no encontrado`);
       }
-      user.status= Status.I;
-       await this.userRepository.save(user);
-       return {message:'Usuario eliminado correctamente'};
+      user.status = Status.I;
+      await this.userRepository.save(user);
+      return { message: 'Usuario eliminado correctamente' };
     } catch (error) {
-      if(error.code === 500){
+      if (error.code === 500) {
         throw new InternalServerErrorException('Error al eliminar el usuario');
       }
       throw error;
-      
+
     }
   }
+  async update(userId: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+
+    if (updateUserDto.role) {
+      const role = await this.roleRepository.findOne({ where: { id: updateUserDto.role } });
+      if (!role) {
+        throw new NotFoundException(`Role with ID ${updateUserDto.role} not found`);
+      }
+      user.role = role; // Asignar la entidad de Role al usuario
+    }
+
+    if (updateUserDto.username) {
+      user.username = updateUserDto.username;
+    }
+
+    if (updateUserDto.password) {
+      user.password = updateUserDto.password;
+    }
+
+    if (updateUserDto.cedula) {
+      user.cedula = updateUserDto.cedula;
+    }
+
+    if (updateUserDto.nombre) {
+      user.nombre = updateUserDto.nombre;
+    }
+
+    if (updateUserDto.apellido) {
+      user.apellido = updateUserDto.apellido;
+    }
+
+    return await this.userRepository.save(user);
+  }
+
 }
